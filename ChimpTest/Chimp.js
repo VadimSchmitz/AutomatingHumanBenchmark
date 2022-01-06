9665d84b71c31ce1464e32f4a5a3b84c1eedf12c
Vadim S <di3mpi3@gmail.com>
2022-01-06 15:40:24 -0800

    ChimpTest automation
    how to use:
    option 1:
    create a new tampermonkey script and paste the code inside.
    
    option 2:
    copy paste the code into the console, program will start once the start button of the game is clicked

---
+++ ChimpTest/Chimp.js
@@ -0,0 +1,50 @@
+const startButton = document.getElementsByClassName("css-de05nr e19owgy710")[0];
+startButton.addEventListener("click", (e) => {
+  continueExecution = true;
+  setTimeout(() => {
+    ClickBoxes()
+  }, 1000)
+})
+
+function ClickBoxes() {
+  holder = []
+  parent = document.getElementsByClassName('css-gmuwbf');
+  parentsChildren = parent[0].children[0].children;
+
+  let gameloop = 0
+  parentsChildren.forEach(element => {
+    element.children.forEach(box => {
+      holder.push(box)
+      if (parseInt(box.getAttribute('data-cellnumber')) > gameloop) {
+        gameloop = parseInt(box.getAttribute('data-cellnumber'))
+        console.log(box.getAttribute('data-cellnumber'))
+      }
+    })
+  });
+
+  console.log(gameloop)
+  for (let i = 0; i < gameloop + 1; i++) {
+    holder.forEach(element => {
+      if (element.classList.contains('css-19b5rdt') || element.classList.contains('css-10qtjsi')) {
+        if (element.getAttribute('data-cellnumber') == i) {
+          console.log(element)
+          element.click()
+        }
+      }
+    });
+  }
+}
+
+document.addEventListener('DOMNodeInserted', (e) => {
+  if (e.target.innerText == "Continue") {
+    console.log(e.target.children[0])
+
+    setTimeout(() => {
+      e.target.children[0].click()
+      setTimeout(() => {
+        ClickBoxes()
+      }, 1000)
+    }, 50)
+  }
+})
+
