1bc7efad15e0eddd1d4c20805635820090d90516
Vadim Schmitz <di3mpi3@gmail.com>
2022-01-24 03:45:05 +0100

    initializing game filed and stuff

--- SequenceMemoryTest/Sequence.js
+++ SequenceMemoryTest/Sequence.js
@@ -1,11 +1,67 @@
-console.log("Sequence.js");	
+console.log("Sequence.js");
+let sequence = [];
+let listening = true;
+let sequenceNum = 1;
 
-const startButton = document.getElementsByClassName("css-de05nr e19owgy710");
-console.log(startButton);
+ function initField() {
+  let squareRow = document.getElementsByClassName('squares')[0].childNodes;
+  let squares = [];
 
-document.addEventListener("onmousedown", (e) => {
-  console.log(e.target);
+  for (let i = 0; i < squareRow.length; i++) {
+    for (let j = 0; j < squareRow[i].childNodes.length; j++) {
+      squares.push(squareRow[i].children[j]);
+    }
+  }
+
+  //create mutation listener for each square
+  squares.forEach(element => {
+    let config = { attributes: true, childList: true, subtree: true };
+    const elementCallback = function (mutationsList, elementObserver) {
+      if (element.classList.contains('active')) {
+        sequence.push(element);
+        console.log("sequence", sequence)
+      }
+    };
+
+    let elementObserver = new MutationObserver(elementCallback);
+    elementObserver.observe(element, config);
+  });
+
+  clicking()
+}
+
+//settimeout for 1 second
+
+function clicking(){
+  console.log("reaching?")
+  console.log(sequence.length>sequenceNum)
+  while(sequence.length > sequenceNum){
+    console.log("listening")
+  }
+  setTimeout(() => {
+  }, 50);
+}
+
+function main() {
+  console.log('run')
+  
+  let mainscreen = document.getElementsByClassName('anim-slide-fade-in')[0];
+  const config = { attributes: true, childList: true, subtree: true };
+  
+  // Callback function to execute when mutations are observed
+  const mainScreenCallback = function (mutationsList, mainScreenObserver) {
+    initField()
+    mainScreenObserver.disconnect();
+  };
+  
+  const mainScreenObserver = new MutationObserver(mainScreenCallback);
+  mainScreenObserver.observe(mainscreen, config);
+}
+
+
+//onpage load
+window.addEventListener('load', function () {
+  main()
 })
-
 
 
